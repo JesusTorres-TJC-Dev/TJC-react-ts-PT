62c994afad68c96719341dc3b892fdf5f5050758
JesusTorres-TJC-Dev <TJC.dev.1997@gmail.com>
2022-11-18 19:50:02 -0400

    fix code in customdialog

--- src/components/CustomDialog/CustomDialog.tsx
+++ src/components/CustomDialog/CustomDialog.tsx
@@ -2,15 +2,15 @@ import { SubjectManager } from '@/models';
 import Dialog from '@mui/material/Dialog';
 import { useEffect, useState } from 'react';
 import { Subscription } from 'rxjs';
-
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
+export interface CustomDialog extends Props {}
 
 export const dialogOpenSubject$ = new SubjectManager<boolean>();
 export const dialogCloseSubject$ = new SubjectManager<boolean>();
 
-export const CustomDialog = ({ children }: Props) => {
+export const CustomDialog : React.FC<CustomDialog> = ({ children }: Props) => {
   const [open, setOpen] = useState(false);
   let openSubject$ = new Subscription();
   let closeSubject$ = new Subscription();
